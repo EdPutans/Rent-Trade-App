95842417157a86bb5ba33bdd48773def3ad01d9a
Ed Putans <edputans@gmail.com>
2018-10-22 15:36:16 +0100

    created controllers and routes

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,31 @@
+class ListingsController < ApplicationController
+#add routes for favorites and relevants
+
+  def index
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def favourite
+  end
+
+  def relevant
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:index, :show, :edit, :update, :create]
+  resources :listings
+  get '/listings/favourite', to: 'listings#favourite'
+  get '/listings/relevant', to: 'listings#relevant'
 end
