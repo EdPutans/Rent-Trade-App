22bbeec25ab4702b25cda5de3b11c5e3acbf40e0
Ed Putans <edputans@gmail.com>
2018-10-24 17:06:22 +0100

    now users able to add multiple pictures

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,26 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root 'users#index'
+
   resources :users, only: [:index, :show, :edit, :update, :create, :destroy]
+  resources :pictures, only: [:create, :show]
   resources :listings
+
   get 'relevant', to: 'listings#relevant'
   get 'favourites', to: 'listings#favourites'
+
   post 'favourites', to: 'listings#add_to_favourites'
   delete 'favourites/', to: 'listings#remove_favourite'
+
   get 'about', to: 'listings#about'
+
   get 'postcodes', to: 'listings#postcodes'
-  get 'my_listings', to: 'listings#my_listings'
   post 'edit_postcodes', to: 'listings#edit_postcodes'
-  # '
-  # get 'listings/favourites', :to => 'listings#favourites'
+
+  get 'my_listings', to: 'listings#my_listings'
+
+  get 'new_picture/:id', to: 'listings#new_pic'
+  post 'create_picture/:id', to:'listings#create_pic'
+
 end
